a9874e8e7c1039ab3470620d40e5faae594f8f6e
nik9anup <108991828+nik9anup@users.noreply.github.com>
2024-07-24 10:07:42 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
 from setuptools import setup, find_packages
 
+# Function to read the requirements.txt file
+def parse_requirements(filename):
+    with open(filename, 'r') as file:
+        return [line.strip() for line in file if line.strip() and not line.startswith('#')]
+
 setup(
-    name='CORE_llm',
+    name='my_project',
     version='0.1',
     packages=find_packages(),
+    install_requires=parse_requirements('requirements.txt'),
 )
