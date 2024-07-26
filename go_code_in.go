cca89fad4cce40aab521d5bb425c5f59b93e956f
Vishal Srinivasa <vishal@VISHAL>
2024-07-26 10:39:14 +0530

    eval code 1 new

--- go_code_in.go
+++ go_code_in.go
@@ -1,61 +1,30 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
+import "fmt"
 
-func readLines(filename string) ([]string, error) {
-	file, err := os.Open(filename)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	var lines []string
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-	return lines, scanner.Err()
-}
-
-// manually implement a sorting function
-func sortStrings(strs []string) {
-	for i := 0; i < len(strs); i++ {
-		for j := i + 1; j < len(strs); j++ {
-			if strs[i] > strs[j] {
-				strs[i], strs[j] = strs[j], strs[i]
+// manually implementing contains function
+func contains(str, substr string) bool {
+	for i := 0; i <= len(str)-len(substr); i++ {
+		j := 0
+		for ; j < len(substr); j++ {
+			if str[i+j] != substr[j] {
+				break
 			}
 		}
-	}
-}
-
-// manually remove duplicates
-func removeDuplicates(strs []string) []string {
-	var result []string
-	encountered := map[string]bool{}
-	for _, str := range strs {
-		if !encountered[str] {
-			encountered[str] = true
-			result = append(result, str)
+		if j == len(substr) {
+			return true
 		}
 	}
-	return result
+	return false
 }
 
 func main() {
-	lines, err := readLines("input.txt")
-	if err != nil {
-		fmt.Println("Error reading file:", err)
-		return
-	}
+	str := "Hello, world!"
+	substr := "world"
 
-	sortStrings(lines)
-	lines = removeDuplicates(lines)
-
-	for _, line := range lines {
-		fmt.Println(line)
+	if contains(str, substr) {
+		fmt.Println("Substring found!")
+	} else {
+		fmt.Println("Substring not found.")
 	}
 }
--- go_code_out.go
+++ go_code_out.go
@@ -1,51 +1,17 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"sort"
 	"strings"
 )
 
 func main() {
-	lines, err := readLines("input.txt")
-	if err != nil {
-		fmt.Println("Error reading file:", err)
-		return
-	}
+	str := "Hello, world!"
+	substr := "world"
 
-	sort.Strings(lines)
-	lines = removeDuplicates(lines)
-
-	for _, line := range lines {
-		fmt.Println(line)
+	if strings.Contains(str, substr) {
+		fmt.Println("Substring found!")
+	} else {
+		fmt.Println("Substring not found.")
 	}
-}
-
-func readLines(filename string) ([]string, error) {
-	file, err := os.Open(filename)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	var lines []string
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-	return lines, scanner.Err()
-}
-
-func removeDuplicates(strs []string) []string {
-	encountered := make(map[string]bool, len(strs))
-	result := make([]string, 0, len(strs))
-	for _, str := range strs {
-		if !encountered[str] {
-			encountered[str] = true
-			result = append(result, str)
-		}
-	}
-	return result
 }
